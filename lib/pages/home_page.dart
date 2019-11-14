b7b03cd7070a667c74e79cfccfb98ecfa8c47d12
sdnuliu <sdnuliu@qq.com>
2019-11-15 00:17:29 +0800

    增加轮播，测试http请求

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+const APPBAR_SCROLL_OFFSET = 100;
 
 ///首页
 class HomePage extends StatefulWidget {
@@ -7,12 +10,83 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List _image_urls = [
+    'https://img1.17img.cn/otherfile/images/201911/ykt/0f812ae8-5b09-4784-94b6-d042cfef3d55.jpg',
+    'https://img1.17img.cn/otherfile/images/201909/ykt/dd54fd65-bfc5-4a49-909a-6f75f87440c7.jpg',
+    'https://img1.17img.cn/otherfile/images/201911/ykt/f5530e71-c293-44dd-91d2-ac8b6d19517a.jpg',
+    'https://img1.17img.cn/otherfile/images/201910/ykt/d7895f47-5a10-41af-9db3-1d6928346480.jpg',
+    'https://img1.17img.cn/otherfile/images/201910/ykt/26664a53-56e7-441b-8d08-2f084a2d1bda.jpg'
+  ];
+  double _appbarAlpha = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('首页'),
+      body: Stack(
+        children: <Widget>[
+          MediaQuery.removePadding(
+              context: context,
+              removeTop: true,
+              child: NotificationListener(
+                  onNotification: (scrollNotification) {
+                    //只监听listview的滚动
+                    if (scrollNotification is ScrollUpdateNotification &&
+                        scrollNotification.depth == 0) {
+                      _onScroll(scrollNotification.metrics.pixels);
+                    }
+                    return false;
+                  },
+                  child: ListView(
+                    children: <Widget>[
+                      Container(
+                        height: 160,
+                        child: Swiper(
+                          itemCount: _image_urls.length,
+                          autoplay: true,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Image.network(
+                              _image_urls[index],
+                              fit: BoxFit.cover,
+                            );
+                          },
+                          pagination: SwiperPagination(),
+                        ),
+                      ),
+                      Container(
+                        height: 800,
+                        child: ListTile(
+                          title: Text('内容待开发，待完善'),
+                        ),
+                      )
+                    ],
+                  ))),
+          Opacity(
+            opacity: _appbarAlpha,
+            child: Container(
+              height: 70,
+              decoration: BoxDecoration(color: Colors.white),
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Text('首页'),
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
+  }
+
+  void _onScroll(double offset) {
+    double alpha = offset / APPBAR_SCROLL_OFFSET;
+    if (alpha < 0) {
+      alpha = 0;
+    } else if (alpha > 1) {
+      alpha = 1;
+    }
+   setState(() {
+     _appbarAlpha = alpha;
+   });
   }
 }
--- lib/pages/mine_page.dart
+++ lib/pages/mine_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_instrument/bean/test_http';
 
 ///我的页
 class MinePage extends StatefulWidget {
@@ -7,12 +10,36 @@ class MinePage extends StatefulWidget {
 }
 
 class _MinePageState extends State<MinePage> {
+  String _result = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('我的'),
-      ),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          InkWell(
+            onTap: () {
+              fetchPost().then((CommonModel value) {
+                setState(() {
+                  _result =
+                      '请求结果：\nhideAppBar：${value.hideAppBar}\nicon：${value.icon}';
+                });
+              });
+            },
+            child: Text('点我发起一次请求'),
+          ),
+          Text(_result)
+        ],
+      )),
     );
+  }
+  ///发起一次请求
+  Future<CommonModel> fetchPost() async {
+    final response = await http
+        .get('http://www.devio.org/io/flutter_app/json/test_common_model.json');
+    final result = json.decode(response.body);
+    return CommonModel.fromJson(result);
   }
 }
